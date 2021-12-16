bdb7773c933362d9a3aba4ec7c9179ae2308115c
Jackson Roberts <jrobe3221@gmail.com>
2021-12-16 13:57:07 -0600

    feat(workflow): implement test workflow to test code

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   extends: ["@commitlint/config-conventional"],
   rules: {
-    "scope-enum": [2, "always", ["global", "general", "package"]],
+    "scope-enum": [2, "always", ["global", "general", "package", "workflow"]],
   },
 };
